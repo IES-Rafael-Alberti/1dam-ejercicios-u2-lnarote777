a302d17b0fa31c5cb977706d92b442e865146792
lnarote777 <nolara@iesrafaelalberti.es>
2023-10-30 21:36:08 +0100

    prueba unitaria ej2

--- tests/test_ej2_02.py
+++ tests/test_ej2_02.py
@@ -1,20 +1,20 @@
 import pytest
-from src.ej2_02 import comporbarPassword
+from src.ej2_02 import comprobarPassword
 
 
 def test_comprobarPassword():
-    assert comporbarPassword("contraseña") == True
-    assert comporbarPassword("ConTRAseña") == True
-    assert comporbarPassword("pedroPo") == False
+    assert comprobarPassword("contraseña") == "Tu contraseña coincide con la mía."
+    assert comprobarPassword("ConTRAseña") == "Tu contraseña coincide con la mía."
+    assert comprobarPassword("pedroPo") == "No coincide."
     
     
 @pytest.mark.parametrize(
     "input_n, expected",
     [
-        ("contraseña", True),
-        ("ConTRaseña", True),
-        ("pedroPo", False),
+        ("contraseña", "Tu contraseña coincide con la mía."),
+        ("ConTRAseña", "Tu contraseña coincide con la mía."),
+        ("pedroPo", "No coincide.")
     ]
 )
-def comporbarPassword(input_n,expected):
-    assert mayorEdad(input_n) == expected
+def test_comprobarPassword(input_n, expected):
+    assert comprobarPassword(input_n) == expected
